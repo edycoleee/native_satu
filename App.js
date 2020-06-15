363cde807c9bea5e806a9894c7117162e1f93f92
edy kholid <edycoleee@gmail.com>
2020-06-15 11:55:54 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return <Text>EdyCole</Text>;
+};
+
+export default App;
